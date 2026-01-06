0801b17c15558e4dd59b42a4828b01fac356bb24
Kulsum <kulsumalam1@gmail.com>
2026-01-06 20:51:37 +0530

    Dont judge me...!!

---
+++ type-casting.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class typecasting{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float num = 99.999f;
+        int sum = (int) num;
+        System.out.println(sum);
+    }
+
+}
