997a934ebdc607972fd23ac88f226ed01b662f51
Kulsum <kulsumalam1@gmail.com>
2026-01-08 19:23:27 +0530

    conditional Statements??

---
+++ conditional-Statements/q2.java
@@ -0,0 +1,16 @@
+// Question2: Finish the following code so that it prints You have a fever if your  temperature is above 100 and otherwise prints You don't have a fever
+
+import java.util.*;
+
+public class q2{
+    public static void main(String args[]){
+        double temp =103.5;
+        if(temp >= 100){
+            System.out.println("You have a feaver!");
+        }
+        else{
+            System.out.println("Chill You dont have a fever!");
+        }
+    }
+
+}
