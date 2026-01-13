2340b7ad1d79306a836de1723fc1511c18506d92
Kulsum <kulsumalam1@gmail.com>
2026-01-13 19:20:15 +0530

    condition-statements

--- conditional-Statements/q4.java
+++ conditional-Statements/q4.java
@@ -2,6 +2,6 @@ public class q4{
     public static void main(String args[]) {
         int a=63, b=36; 
         boolean x= (a<b)?true:false;
-        int y= (a>b)?a:b;
+        int y = (a>b)?a:b;
     }
 }
